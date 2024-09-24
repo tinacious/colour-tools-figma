b0b1fb7794773b9ab4753e9e2439989b9674ba82
@tinacious <info@tinaciousdesign.com>
2024-09-24 19:58:18 -0400

    create variables

--- code.ts
+++ code.ts
@@ -34,8 +34,6 @@ figma.ui.onmessage = async (message) => {
       return createColourPalette(message.data)
     case 'refresh-local-styles':
       return loadLocalStyles()
-    case 'graph':
-      return createGraphWithNumbers(message.data)
     default:
       console.log('Unsupported message', message)
   }
@@ -63,6 +61,8 @@ async function createColourPalette(data: CreatePaletteRequest) {
   frame.layoutMode = 'HORIZONTAL'
   frame.layoutSizingHorizontal = 'HUG'
 
+  const variableCollection = figma.variables.createVariableCollection(paletteName)
+
   const nodes: SceneNode[] = [];
 
   colours.forEach(async (colour) => {
@@ -80,14 +80,29 @@ async function createColourPalette(data: CreatePaletteRequest) {
     ]
 
     // Create styles
+    const colourName = paletteName + '/' + name
+    const solidPaint = figma.util.solidPaint(hexToFigmaRGB(hex), {
+      color: hexToFigmaRGB(hex)
+    })
+
+    // Create colours
     const paintStyle = figma.createPaintStyle()
-    paintStyle.name = paletteName + '/' + name
+    paintStyle.name = colourName
     paintStyle.paints = [
-      {
-        type: 'SOLID',
-        color: hexToFigmaRGB(hex),
-      }
+      solidPaint
     ]
+    const colourVariable = figma.variables.createVariable(
+      colourName,
+      variableCollection,
+      'COLOR',
+    )
+    figma.variables.setBoundVariableForPaint(
+      solidPaint,
+      'color',
+      colourVariable,
+    )
+    colourVariable.setValueForMode(variableCollection.modes[0].modeId, solidPaint.color)
+
 
     // Assign the style to the rectangle
     await swatch.setFillStyleIdAsync(paintStyle.id)
@@ -107,103 +122,6 @@ async function exportStylesToColourPalette() {
   console.log('styles', styles)
 }
 
-async function createGraphWithNumbers(numbers: number[]) {
-  // Inter Regular is the font that objects will be created with by default in
-  // Figma. We need to wait for fonts to load before creating text using them.
-  await figma.loadFontAsync({ family: "Inter", style: "Regular" })
-
-  const frameWidth = 800
-  const frameHeight = 600
-  const chartX = 25
-  const chartY = 50
-  const chartWidth = frameWidth - 50
-  const chartHeight = frameHeight - 50
-
-  const frame = figma.createFrame()
-  frame.resizeWithoutConstraints(frameWidth, frameHeight)
-
-  // Center the frame in our current viewport so we can see it.
-  frame.x = figma.viewport.center.x - frameWidth / 2
-  frame.y = figma.viewport.center.y - frameHeight / 2
-
-  // Border for the chart
-  const border = figma.createRectangle()
-  frame.appendChild(border)
-  border.resizeWithoutConstraints(frameWidth, frameHeight)
-  border.strokeAlign = 'INSIDE'
-  border.strokeWeight = 3
-  border.fills = []
-  border.strokes = [{ type: 'SOLID', color: { r: 0, g: 0, b: 0 } }]
-  border.constraints = { horizontal: 'STRETCH', vertical: 'STRETCH' }
-
-  // Line at the bottom of the chart
-  const line = figma.createRectangle()
-  frame.appendChild(line)
-  line.x = chartX
-  line.y = chartY + chartHeight
-  line.resizeWithoutConstraints(chartWidth, 3)
-  line.fills = [{ type: 'SOLID', color: { r: 0, g: 0, b: 0 } }]
-  line.constraints = { horizontal: 'STRETCH', vertical: 'STRETCH' }
-
-  const min = numbers.reduce((a, b) => Math.min(a, b), 0)
-  const max = numbers.reduce((a, b) => Math.max(a, b), 0)
-
-  for (let i = 0; i < numbers.length; i++) {
-    const num = numbers[i];
-    const left = chartX + chartWidth * (i + 0.25) / numbers.length;
-    const right = chartX + chartWidth * (i + 0.75) / numbers.length;
-    const top = chartY + chartHeight - chartHeight * (Math.max(0, num) - min) / (max - min);
-    const bottom = chartY + chartHeight - chartHeight * (Math.min(0, num) - min) / (max - min);
-
-    // The column
-    const column = figma.createRectangle()
-    frame.appendChild(column)
-    column.x = left
-    column.y = top
-    column.resizeWithoutConstraints(right - left, bottom - top)
-    column.fills = [{ type: 'SOLID', color: { r: 1, g: 0, b: 0 } }]
-    column.constraints = { horizontal: 'STRETCH', vertical: 'STRETCH' }
-
-    // The label
-    const label = figma.createText()
-    frame.appendChild(label)
-    label.x = left - 50
-    label.y = top - 50
-    label.resizeWithoutConstraints(right - left + 100, 50)
-    label.fills = [{ type: 'SOLID', color: { r: 0, g: 0, b: 0 } }]
-    label.characters = num.toString()
-    label.fontSize = 30
-    label.textAlignHorizontal = 'CENTER'
-    label.textAlignVertical = 'BOTTOM'
-    label.constraints = { horizontal: 'STRETCH', vertical: 'STRETCH' }
-  }
-}
-
-// Make sure to close the plugin when you're done. Otherwise the plugin will
-// keep running, which shows the cancel button at the bottom of the screen.
-// figma.closePlugin();
-
-
-function createRectanglesExample() {
-  // This plugin creates 5 rectangles on the screen.
-  const numberOfRectangles = 5
-
-  // This file holds the main code for plugins. Code in this file has access to
-  // the *figma document* via the figma global object.
-  // You can access browser APIs in the <script> tag inside "ui.html" which has a
-  // full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
-
-  const nodes: SceneNode[] = [];
-  for (let i = 0; i < numberOfRectangles; i++) {
-    const rect = figma.createRectangle();
-    rect.x = i * 150;
-    rect.fills = [{ type: 'SOLID', color: { r: 1, g: 0.5, b: 0 } }];
-    figma.currentPage.appendChild(rect);
-    nodes.push(rect);
-  }
-  figma.currentPage.selection = nodes;
-  figma.viewport.scrollAndZoomIntoView(nodes);
-}
 
 // region Colour utils
 
